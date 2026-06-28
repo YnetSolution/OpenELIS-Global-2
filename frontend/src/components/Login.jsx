faac63512af93d24288e4c18562a3ebbde5e59a3
Michaelndula <michaelndula@gmail.com>
2026-06-29 01:08:35 +0300

    refactor: remove change password button and set login button width to 100% in Login component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -335,30 +335,19 @@ function Login(props) {
                               onFocus={markUserActive}
                               onChange={markUserActive}
                             />
-                            <Stack orientation="horizontal">
-                              <Button
-                                type="submit"
-                                disabled={!isValid}
-                                data-cy="loginButton"
-                              >
-                                <FormattedMessage id="label.button.login" />
-                                <Loading
-                                  small={true}
-                                  withOverlay={false}
-                                  className={submitting ? "show" : "hidden"}
-                                />
-                              </Button>
-
-                              <Button
-                                data-cy="changePassword"
-                                type="button"
-                                onClick={() => {
-                                  window.location.href = "/ChangePasswordLogin";
-                                }}
-                              >
-                                <FormattedMessage id="label.button.changepassword" />
-                              </Button>
-                            </Stack>
+                            <Button
+                              type="submit"
+                              disabled={!isValid}
+                              data-cy="loginButton"
+                              style={{ width: "100%" }}
+                            >
+                              <FormattedMessage id="label.button.login" />
+                              <Loading
+                                small={true}
+                                withOverlay={false}
+                                className={submitting ? "show" : "hidden"}
+                              />
+                            </Button>
                           </>
                         )}
                         {configurationProperties?.useSaml == "true" &&
